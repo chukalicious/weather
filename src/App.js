714d0ce86fe93e314a70872c1b55c1e58a7af1c4
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-10-28 20:21:58 -0700

    tested and added API call to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
+import axios from "axios";
+import { baseURL } from "./helpers/apiCall";
 
 function App() {
   useEffect(() => {
     themeChange(false);
     // 👆 false parameter is required for react project
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `${baseURL}latitude=34.08&longitude=-118.15&hourly=temperature_2m&daily=weathercode,sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FLos_Angeles`
+      )
+      .then((res) => console.log("res: ", res))
+      .catch((err) => console.log(err));
   }, []);
 
   return <div></div>;
---
+++ src/helpers/apiCall.js
@@ -0,0 +1,1 @@
+export const baseURL = "https://api.open-meteo.com/v1/forecast?";
