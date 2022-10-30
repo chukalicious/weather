43c7f9dfbb440cbab58b63597ce54a5d681be174
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-10-29 18:11:08 -0700

    deleted dead code

--- src/components/InputComponent/Input.js
+++ src/components/InputComponent/Input.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Input = () => {
   const [location, setLocation] = useState("");
@@ -32,41 +32,6 @@ const Input = () => {
         </form>
       </div>
     </div>
-
-    // <div className="hero min-h-[70vh] bg-base-200">
-    //   <div className="hero-content text-center">
-    //     <div className="max-w-md">
-    //       <h1 className="text-3xl font-bold pb-4">Enter your location</h1>
-    //       <form className="form-control" onSubmit={handleSubmit}>
-    //         <div className="input-group">
-    //           <input
-    //             value={location}
-    //             onChange={handleChange}
-    //             type="text"
-    //             placeholder="Search…"
-    //             className="input input-bordered"
-    //           />
-    //           <button className="btn btn-square">
-    //             <svg
-    //               xmlns="http://www.w3.org/2000/svg"
-    //               className="h-6 w-6"
-    //               fill="none"
-    //               viewBox="0 0 24 24"
-    //               stroke="currentColor"
-    //             >
-    //               <path
-    //                 strokeLinecap="round"
-    //                 strokeLinejoin="round"
-    //                 strokeWidth="2"
-    //                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-    //               />
-    //             </svg>
-    //           </button>
-    //         </div>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
